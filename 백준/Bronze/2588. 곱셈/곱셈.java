b74b61d8516d28ba7cd4e36ec871419d66b6c08b
Lee Gee Young <69562480+wldud7788@users.noreply.github.com>
2024-05-27 21:53:51 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       
+      int X = sc.nextInt();
+      int Y = sc.nextInt();
+      
+      System.out.println(X*(Y%10));
+      System.out.println(X*(Y/10%10));
+      System.out.println(X*(Y/100));
+      System.out.println(X*Y);
+      
+    }
+}
