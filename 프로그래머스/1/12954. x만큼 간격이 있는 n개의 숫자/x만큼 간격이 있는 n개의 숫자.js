613e590c133f9fec8668733a421f71b152d696b8
Lee Gee Young <69562480+wldud7788@users.noreply.github.com>
2024-07-02 17:58:00 +0900

    Update x만큼 간격이 있는 n개의 숫자.js

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -2,8 +2,7 @@ function solution(x, n) {
     var answer = [];
     var cnt = 0;
     for (var i = 1; i<=n; i++){
-        answer[cnt] = x*i;
-        cnt++;
+        answer[cnt++] = x*i;
     }
     return answer;
 }
