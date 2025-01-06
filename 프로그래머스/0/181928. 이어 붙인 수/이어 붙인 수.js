9b12cae628cd9968985c548906d2e780b572e7cd
Lee Gee Young <rooftop7788@naver.com>
2025-01-07 01:37:15 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,6 +1,6 @@
 function solution(num_list) {
-    let 짝수 = ""
-    let 홀수 = ""
+    let 짝수 = "";
+    let 홀수 = "";
     num_list.map((item)=>item %2 == 0 ? 짝수 += item : 홀수 += item);
     return +짝수 + +홀수;
 }
