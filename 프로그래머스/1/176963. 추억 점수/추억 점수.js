ef7b4d3c6de0ca979320bf782b774b7a132091b1
Lee Gee Young <rooftop7788@naver.com>
2025-01-11 02:09:10 +0900

    [level 1] Title: 추억 점수, Time: 2.41 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,14 +1,13 @@
 function solution(name, yearning, photo) {
-    let score = new Map();
-    let sum = 0;
+    let scoreMap = new Map();
     let result = [];
     for(let i = 0; i<name.length; i++){
-        score.set(name[i],yearning[i]);
+        scoreMap.set(name[i],yearning[i]);
     }
     photo.forEach((item)=>{
         let sum = 0;
         item.forEach((item2)=>{
-            sum += score.get(item2) || 0;
+            sum += scoreMap.get(item2) || 0;
         })
         result.push(sum);
     })
