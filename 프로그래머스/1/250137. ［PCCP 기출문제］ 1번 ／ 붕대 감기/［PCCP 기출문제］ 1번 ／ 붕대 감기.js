0ab28f1a68f62ce77995f442ba7ca46b918fddb9
Lee Gee Young <rooftop7788@naver.com>
2025-01-23 02:55:50 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,33 +1,33 @@
 function solution(bandage, health, attacks) {
     
-    const 공격마지막 = attacks[attacks.length-1] 
-    const 마지막공격시간 = 공격마지막[0] 
+    const lastAttack = attacks[attacks.length-1] 
+    const lastAttackTime = lastAttack[0] 
     
-    let 공격받은후체력 = health; 
-    let 연속카운트 = 0;
+    let currentHelath = health; 
+    let healCount = 0;
     
-    const 공격시간맵 = new Map();
+    const attackTimeMap = new Map();
     attacks.forEach((item)=>{
-      const [공격시간, 피해량] = item;
-      공격시간맵.set(공격시간,피해량)  
+      const [attackTime, damage] = item;
+      attackTimeMap.set(attackTime,damage)  
     })
     
 
-    for(let i = 1; i <= 마지막공격시간; i++) { 
-        if(공격시간맵.get(i)){
-            공격받은후체력 -= 공격시간맵.get(i);
-            연속카운트 = 0;
-            if(공격받은후체력 <= 0){
+    for(let i = 1; i <= lastAttackTime; i++) { 
+        if(attackTimeMap.get(i)){
+            currentHelath -= attackTimeMap.get(i);
+            healCount = 0;
+            if(currentHelath <= 0){
                 return -1;
             }
         } else {
-            공격받은후체력 = Math.min(health, 공격받은후체력+bandage[1]);
-            연속카운트 ++;
-            if(연속카운트 === bandage[0]){
-                공격받은후체력 = Math.min(health, 공격받은후체력+bandage[2]);
-                연속카운트 = 0;
+            currentHelath = Math.min(health, currentHelath+bandage[1]);
+            healCount ++;
+            if(healCount === bandage[0]){
+                currentHelath = Math.min(health, currentHelath+bandage[2]);
+                healCount = 0;
             }
         }
     }
-    return 공격받은후체력;
+    return currentHelath;
 }
