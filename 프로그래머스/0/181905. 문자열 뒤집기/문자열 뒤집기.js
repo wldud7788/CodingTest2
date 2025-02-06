371125e60b24c8beec047e2df9964de011b803c1
Lee Gee Young <rooftop7788@naver.com>
2025-02-06 11:29:07 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.14 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,6 @@
+function solution(my_string, s, e) {
+    const slicedStr = my_string.slice(0,s) + 
+          my_string.slice(s,e+1).split('').reverse().join('')+
+          my_string.slice(e+1)
+    return slicedStr
+}
